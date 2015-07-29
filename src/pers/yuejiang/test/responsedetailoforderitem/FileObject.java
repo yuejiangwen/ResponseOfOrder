f8fd47664eb1bf9e4a7c24987888e7d3d730478c
yuejiangwen <optu@outlook.com>
2015-07-29 12:07:34 +0800

    FileObject

--- src/pers/yuejiang/test/responsedetailoforderitem/FileObject.java
+++ src/pers/yuejiang/test/responsedetailoforderitem/FileObject.java
@@ -1,28 +1,26 @@
-
+package pers.yuejiang.test.responsedetailoforderitem;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class FileObject {
-
 	public static void main(String[] args) {
-		        try {
-		            FileInputStream fileInputStream = new FileInputStream("responseDetailsOfSalesorderItem.json");
-		            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-		            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-		            String s;
-		            //每次读取一行，当改行为空时结束
-		            while((s = bufferedReader.readLine()) != null){
-		                System.out.println("read:" + s);
-		            }
-		            bufferedReader.close();
-		        }
-		        catch(IOException e) {
-		            System.out.println(e);
-		        }
+		try {
+			FileInputStream fileInputStream = new FileInputStream("responseDetailsOfSalesorderItem.json");
+		    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+		    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		    String string; //每次读取一行，当改行为空时结束
+		    
+		    
+		    while((string = bufferedReader.readLine()) != null){
+		    
+		    	System.out.println("read:" + string);
 		    }
-
-		// TODO Auto-generated method stub
-
+		    bufferedReader.close();
+		    }
+		catch(IOException e) {
+			System.out.println(e);
+			}
+		}
 	}
