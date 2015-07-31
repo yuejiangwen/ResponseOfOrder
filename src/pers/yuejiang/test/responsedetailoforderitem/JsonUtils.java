e9751c26c5899f02eaeafbd7ef30dd39eca0f728
yuejiangwen <optu@outlook.com>
2015-07-31 09:48:55 +0800

    FunctionFinished

--- src/pers/yuejiang/test/responsedetailoforderitem/JsonUtils.java
+++ src/pers/yuejiang/test/responsedetailoforderitem/JsonUtils.java
@@ -2,7 +2,6 @@ package pers.yuejiang.test.responsedetailoforderitem;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,15 +16,31 @@ import net.sf.json.JSONObject;
 
 public class JsonUtils {
 
-	public static String readFile(String path) throws FileNotFoundException {
+	/**
+	 * 输出文件
+	 * @param path 输出路径
+	 * @param data 数据
+	 * @throws IOException
+	 */
+	public static void writeFile(String path,String data) throws IOException{
+		Writer write = new FileWriter(path);  
+	    write.write(data);  
+	    write.flush();  
+	    write.close(); 
+	}
+	/**
+	 * 读取文件
+	 * @param path 路径
+	 * @return
+	 */
+	public static String readFile(String path) {
 		File file = new File(path);
-		FileReader fileReader = new FileReader(file);
 		BufferedReader bufferedReader = null;
 		String result = "";
 		try {
-			bufferedReader = new BufferedReader(fileReader);// * 用Reader中的read方法读取字符，用字符流读取
+			bufferedReader = new BufferedReader(new FileReader(file));
 			String tempString = null;
-			// 一次读取一行，直到读入null为文件结尾
+			// 一次读入一行，直到读入null为文件结束
 			while ((tempString = bufferedReader.readLine()) != null) {
 				result = result + tempString;
 			}
@@ -37,31 +52,92 @@ public class JsonUtils {
 	}
 	
 	/**
-	 * 获取查询条件下jsonObject的值
+	 * 获取查询条件下jsonObject的数量
+	 * @param obj json对象
+	 * @param queryKey 查询的key
+	 * @param queryValue 查询的值
+	 * @return
+	 * @Exception JSONException
 	 */
-	
+	@SuppressWarnings("unchecked")
+	public static int getJSONObjDstNum(JSONObject obj,String queryKey,String queryValue) throws JSONException {
+		int count = 0;
+		Iterator<String> iterator = obj.keys();
+		while (iterator.hasNext()) {
+			String key = iterator.next();
+			if (key.equals(queryKey)) {
+				try {
+					
+					if (obj.getString(key).equals(queryValue)) {
+						count++;
+					}
+					continue; 
+				} catch (JSONException e) {
+					e.printStackTrace();
+				} finally {
+				}
+			}
+			//递归（递归存在两种形式，JSONArray和JSONObject）
+			Object sub_obj = obj.get(key);
+			count += getObjDstNum(sub_obj,queryKey,queryValue);
+		}
+		return count;
+	}
+	public static int getObjDstNum(Object obj,String queryKeyName,String queryKeyValue) throws JSONException {
+		try {
+			JSONArray object = (JSONArray) obj;
+			if (object != null) {
+				return getJSONArrayDstNum(object,queryKeyName,queryKeyValue);
+			}
+		} catch (ClassCastException e) {
+		} finally {
+		}
+
+		try {
+			JSONObject object = (JSONObject) obj;
+			if (object != null) {
+				return getJSONObjDstNum(object,queryKeyName,queryKeyValue);
+			}
+		} catch (ClassCastException e) {
+		} finally {
+		}
+		return 0;
+	}
+	public static int getJSONArrayDstNum(JSONArray array,String queryKeyName,String queryKeyValue) throws JSONException {
+		int count = 0;
+		int len = array.size();
+		for (int i = 0; i < len; i++) {
+			Object sub_obj = array.get(i);
+			count += getObjDstNum(sub_obj,queryKeyName,queryKeyValue);
+		}
+		return count;
+	}
 	
 	/**
-	 * 条件获取jsonObject	 
+	 * 条件获取jsonObject
+	 * @param mainObj 要查询的json对象
+	 * @param key 一级key
+	 * @param queryString 条件（格式为：aaaa=123,bbb=321）
+	 * @return
 	 */
-	public static List<JSONObject> getJsonObjectByQuery(JSONObject jsonObject,String key,String inqueryString){
+	public static List<JSONObject> getJsonObjectByQuery(JSONObject mainObj,String tagetKeyName,String queryString){
 		List<JSONObject> resultObj = new ArrayList<JSONObject>();
-		if(inqueryString==null){
+		if(queryString==null){
 			return resultObj;
 		}
-		//获取指定key的JSON数组
-		JSONArray jsonArray = jsonObject.getJSONArray(key);
+		//获取指定第一级key的JSON数组
+		JSONArray jsonArray = mainObj.getJSONArray(tagetKeyName);
 		//分割查询条件
-		String[] inqueryArray = inqueryString.split(",");
+		String[] queryArray = queryString.split(",");
 		int len = jsonArray.size();
 		for (int i = 0; i < len; i++) {
 			JSONObject subObj = jsonArray.getJSONObject(i);
 			boolean resultBoolean = true;
-			//遍历查询条件，多重条件查询
-			if(inqueryArray.length!=0){
-				for (String inqueryS : inqueryArray) {
-					String name = inqueryS.split("=")[0];
-					String value = inqueryS.split("=")[1];
+			//遍历查询条件，条件联合查询
+			if(queryArray.length!=0){
+				for (String querySplit : queryArray) {
+					String name = querySplit.split("=")[0];
+					String value = querySplit.split("=")[1];
 					String item = subObj.getString(name);
 					resultBoolean = resultBoolean && item.equals(value);
 				}
