a880ff45468ed6a521ef1138b23b2123d172a1e7
yuejiangwen <optu@outlook.com>
2015-08-01 23:05:10 +0800

    CodeEdited

--- src/pers/yuejiang/test/responsedetailoforderitem/FileObject.java
+++ src/pers/yuejiang/test/responsedetailoforderitem/FileObject.java
@@ -8,35 +8,35 @@ import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 import net.sf.json.util.JSONTokener;
 
+/** 三个功能：JSON节点查询、计数、 修改.*/
 public class FileObject {
-	public static void main(String[] args) throws JSONException, IOException {
+		public static void main(final String[] args) throws JSONException, IOException {
 		//读取文件
 		String json = JsonUtils.readFile("./responseDetailsOfSalesorderItem.json");//responseDetailsOfSalesorderItem
 		JSONTokener jsonParser = new JSONTokener(json);
 		JSONObject mainObj = (JSONObject) jsonParser.nextValue();
-		
+
 		System.out.println("a.提取出SD_DOC=0000000151, PARTN_ROLE=AG的ORDER_PARTNERS_OUT子节点");
-		List<JSONObject> resultObj = JsonUtils.getJsonObjectByQuery(mainObj, "ORDER_PARTNERS_OUT","SD_DOC=0000000151,PARTN_ROLE=AG");
+		List<JSONObject> resultObj = JsonUtils.getJsonObjectByQuery(mainObj, "ORDER_PARTNERS_OUT", "SD_DOC=0000000151,PARTN_ROLE=AG");
 
 		System.out.println("\nb.统计一下SD_DOC=0000000151一共出现了几次");
 		int count = JsonUtils.getJSONObjDstNum(mainObj, "SD_DOC", "0000000151");
-        System.out.println("SD_DOC=0000000151一共出现了"+count+"次");
-		
-        
+        System.out.println("SD_DOC=0000000151一共出现了" + count + "次");
+
         System.out.println("\nc.将SD_DOC=0000000151, PARTN_ROLE=AG的ORDER_PARTNERS_OUT子节点的LEVEL_NR设置为100, 并将结果保存在另一个json文件中");
 		String resultJson = "";
         for (JSONObject subObj : resultObj) {
-			System.out.println("修改前："+subObj);
+			System.out.println("修改前："+ subObj);
 			//修改已存在数据的节点
-			subObj.element("LEVEL_NR", 100);
-			System.out.println("修改后："+subObj);
-			resultJson+=subObj.toString();
+			subObj.element("LEVEL_NR", 100);   //修改节点数值为100
+			System.out.println("修改后："+ subObj);
+			resultJson += subObj.toString();
 		}
         //输出路径
 		String path = "./responseDetailsOfSalesorderItem2.json"; //responseDetailsOfSalesorderItem
-		System.out.println("输出到文件："+path);
+		System.out.println("输出到文件：" + path);
 		JsonUtils.writeFile(path, resultJson);
-		
+
 	}
 }
 
--- src/pers/yuejiang/test/responsedetailoforderitem/JsonUtils.java
+++ src/pers/yuejiang/test/responsedetailoforderitem/JsonUtils.java
@@ -1,7 +1,7 @@
 package pers.yuejiang.test.responsedetailoforderitem;
 
 import java.io.BufferedReader;
-import java.io.File;
+//import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,21 +14,23 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 
+
+/**JSON文件节点查询操作工具类. */
 public class JsonUtils {
 
 	/**
 	 * 读取文件
-	 * @param path 路径
-	 * @return
+	 * @param readpath 路径
+	 * @return result
 	 */
-	public static String readFile(String path) {
-		File file = new File(path);
+	public static String readFile(final String readpath) {
+		//File file = new File(path);
 		BufferedReader bufferedReader = null;
 		String result = "";
 		try {
-			bufferedReader = new BufferedReader(new FileReader(file));
+			bufferedReader = new BufferedReader(new FileReader(readpath)); //取消新建File实例
 			String tempString = null;
-			while ((tempString = bufferedReader.readLine()) != null) {   // 一次读入一行，直到读入null为文件结束
+			while ((tempString = bufferedReader.readLine()) != null) {     // 一次读入一行，直到读入null为文件结束
 				result = result + tempString;
 			}
 			bufferedReader.close();
@@ -37,30 +39,30 @@ public class JsonUtils {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * 输出文件
-	 * @param path 输出路径
+	 * @param writepath 输出路径
 	 * @param data 数据
 	 * @throws IOException
 	 */
-	public static void writeFile(String path,String data) throws IOException{
-		Writer write = new FileWriter(path);  
-	    write.write(data);  
-	    write.flush();  
-	    write.close(); 
+	public static void writeFile(final String writepath, final String data) throws IOException {
+		Writer write = new FileWriter(writepath);
+	    write.write(data);
+	    write.flush();
+	    write.close();
 	}
-	
+
 	/**
 	 * 条件获取jsonObject
 	 * @param mainObj 要查询的json对象
 	 * @param tagetKeyName 目标查询key
-	 * @param queryString 
-	 * @return
+	 * @param queryString
+	 * @return resultObj
 	 */
-	public static List<JSONObject> getJsonObjectByQuery(JSONObject mainObj,String tagetKeyName,String queryString){
+	public static List<JSONObject> getJsonObjectByQuery(final JSONObject mainObj, final String tagetKeyName, final String queryString) {
 		List<JSONObject> resultObj = new ArrayList<JSONObject>();
-		if(queryString==null){
+		if (queryString == null) {
 			return resultObj;
 		}
 		//获取指定key的JSON数组
@@ -72,7 +74,7 @@ public class JsonUtils {
 			JSONObject subObj = jsonArray.getJSONObject(i);
 			boolean resultBoolean = true;
 			//遍历查询条件，多重条件查询
-			if(queryArray.length!=0){
+			if (queryArray.length != 0) {
 				for (String querySplit : queryArray) {
 					String name = querySplit.split("=")[0];
 					String value = querySplit.split("=")[1];
@@ -81,51 +83,52 @@ public class JsonUtils {
 				}
 			}
 			//得到符合条件的json对象
-			if(resultBoolean){
+			if (resultBoolean) {
 				System.out.println(subObj);
 				resultObj.add(subObj);
 			}
 		}
 		return resultObj;
 	}
-	
+
 	/**
 	 * 获取查询条件下jsonObject的数量
-	 * @param obj json对象
+	 * @param jsonObj json对象
 	 * @param queryKeyName 查询的key
 	 * @param queryKeyValue 查询的值
-	 * @return
+	 * @return count
 	 * @Exception JSONException
 	 */
 	//计算JSONObject中出现目标值次数
-	public static int getJSONObjDstNum(JSONObject jsonObj,String queryKeyName,String queryKeyValue) throws JSONException {
+	@SuppressWarnings("unchecked")
+	public static int getJSONObjDstNum(final JSONObject jsonObj, final String queryKeyName, final String queryKeyValue) throws JSONException {
 		int count = 0;
 		Iterator<String> iterator = jsonObj.keys();
 		while (iterator.hasNext()) {
 			String key = iterator.next();
 			if (key.equals(queryKeyName)) {
 				try {
-					
+
 					if (jsonObj.getString(key).equals(queryKeyValue)) {
 						count++;
 					}
-					continue; 
+					continue;
 				} catch (JSONException e) {
 					e.printStackTrace();
-				} 
+				}
 			}
 			//递归（递归存在两种形式，JSONArray和JSONObject）
 			Object sub_obj = jsonObj.get(key);
-			count += getObjDstNum(sub_obj,queryKeyName,queryKeyValue);
+			count += getObjDstNum(sub_obj, queryKeyName, queryKeyValue);
 		}
 		return count;
 	}
 	//判断对象属于object还是array
-	public static int getObjDstNum(Object obj,String queryKeyName,String queryKeyValue) throws JSONException {
+	public static int getObjDstNum(final Object obj,final String queryKeyName, final String queryKeyValue) {
 		try {
 			JSONArray object = (JSONArray) obj;
 			if (object != null) {
-				return getJSONArrayDstNum(object,queryKeyName,queryKeyValue);
+				return getJSONArrayDstNum(object, queryKeyName, queryKeyValue);
 			}
 		} catch (ClassCastException e) {
 		}
@@ -133,19 +136,19 @@ public class JsonUtils {
 		try {
 			JSONObject object = (JSONObject) obj;
 			if (object != null) {
-				return getJSONObjDstNum(object,queryKeyName,queryKeyValue);
+				return getJSONObjDstNum(object, queryKeyName, queryKeyValue);
 			}
 		} catch (ClassCastException e) {
-		} 
+		}
 		return 0;
 	}
 	//计算JSONArray中出现目标值次数
-	public static int getJSONArrayDstNum(JSONArray array,String queryKeyName,String queryKeyValue) throws JSONException {
+	public static int getJSONArrayDstNum(final JSONArray array, final String queryKeyName, final String queryKeyValue) {
 		int count = 0;
 		int len = array.size();
 		for (int i = 0; i < len; i++) {
 			Object sub_obj = array.get(i);
-			count += getObjDstNum(sub_obj,queryKeyName,queryKeyValue);
+			count += getObjDstNum(sub_obj, queryKeyName, queryKeyValue);
 		}
 		return count;
 	}
